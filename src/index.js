5a67f35c46b8f93a172a14b03b575de4a5e1a6b7
Mari Komardina <fox_kom@internet.ru>
2022-01-09 20:20:29 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let num = String(Math.abs(n));
+  let result = '';
+  for (let i = num.length - 1; i >= 0; i--) {
+    result += num[i];
+  }
+  return Number(result);
 }
